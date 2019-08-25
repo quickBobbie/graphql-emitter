8a37754a2f0550169fa7b6c67448359b30196c92
bobbie <bobbie@bobbies-MacBook-Pro.local>
2019-08-26 01:08:44 +0400

    Create lib

---
+++ index.js
@@ -0,0 +1,54 @@
+const {
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLObjectType
+} = require("graphql");
+
+module.exports = (function(objectType) {
+    const OBJECT_TYPE = objectType;
+
+    let query = { name: "Query", fields: {} };
+    let mutation = { name: "Mutation", fields: {} };
+
+    const emitter = (type, eventString, args, callback) => {
+        let [isList, event] = parseEvent(eventString);
+
+        let obj = {
+            type: isList ? GraphQLList(OBJECT_TYPE) : OBJECT_TYPE,
+            args: args,
+            resolve: callback
+        };
+
+        if (!obj.args || typeof obj.args !== "object" || Object.keys(obj.args).length === 0) {
+            delete obj.args;
+        }
+        if (type === "query") {
+            query.fields[event] = obj;
+        } else if (type === "mutation") {
+            mutation.fields[event] = obj;
+        }
+    };
+
+    const parseEvent = (eventString) => {
+        let isList = eventString.indexOf("[]") !== -1;
+
+        return [isList, eventString.replace(/\[]/gi, '')];
+    };
+
+    this.query = (event, args, callback) => {
+        emitter("query", event, args, callback);
+    };
+
+    this.mutation = (event, args, callback) => {
+        emitter("mutation", event, args, callback);
+    };
+
+    this.export = () => {
+        let config = {
+            query: new GraphQLObjectType(query),
+            mutation: new GraphQLObjectType(mutation)
+        };
+
+        return new GraphQLSchema(config);
+    }
+});
